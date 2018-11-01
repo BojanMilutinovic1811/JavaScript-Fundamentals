93f95c832011e35eff97e9c69bf0d2e82421c488
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-11-01 09:39:00 +0100

    member since slice

--- gitHubFinder/ui.js
+++ gitHubFinder/ui.js
@@ -4,25 +4,30 @@ class Ui {
     }
 
     displayUser(user) {
+
+        const {avatar_url, html_url, public_repos, public_gists, followers, following, company, blog, location, created_at} = user.profile; 
+
+        const memberSince = created_at.slice(0,10)
+        
         let output = '';
         this.profile.innerHTML = `
             <div class='card card-body mb-3'>
                 <div class='row'>
                     <div class='col-md-3'>
-                        <img class='img-fluid mb-2' src='${user.profile.avatar_url}'>
-                        <a href='${user.profile.html_url}' target='_blank' class='btn btn-block btn-primary mb-3'>View Profile</a>
+                        <img class='img-fluid mb-2' src='${avatar_url}'>
+                        <a href='${html_url}' target='_blank' class='btn btn-block btn-primary mb-3'>View Profile</a>
                     </div>
                     <div class='col-md-9'>
-                        <span class='badge badge-primary'>Public Repos: ${user.profile.public_repos}</span>
-                        <span class='badge badge-secondary'>Public Gists: ${user.profile.public_gists}</span>
-                        <span class='badge badge-success'>Followers: ${user.profile.followers}</span>
-                        <span class='badge badge-info'>Following: ${user.profile.following}</span>
+                        <span class='badge badge-primary'>Public Repos: ${public_repos}</span>
+                        <span class='badge badge-secondary'>Public Gists: ${public_gists}</span>
+                        <span class='badge badge-success'>Followers: ${followers}</span>
+                        <span class='badge badge-info'>Following: ${following}</span>
                         <br><br>
                         <ul class='list-group'>
-                            <li class='list-group-item'>Company: ${user.profile.company}</li>
-                            <li class='list-group-item'>Blog: ${user.profile.blog}</li>
-                            <li class='list-group-item'>Location: ${user.profile.location}</li>
-                            <li class='list-group-item'>Member since: ${user.profile.created_at}</li>
+                            <li class='list-group-item'>Company: ${company}</li>
+                            <li class='list-group-item'>Blog: ${blog}</li>
+                            <li class='list-group-item'>Location: ${location}</li>
+                            <li class='list-group-item'>Member since: ${memberSince}</li>
                         </ul>
                     </div>
                 </div>
