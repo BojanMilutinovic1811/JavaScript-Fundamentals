3e38e2984add19be7cd13aaf39cc89e4873179bc
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-12-14 14:16:11 +0100

    update

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -6,7 +6,12 @@ const httpAddress = 'http://localhost:3000/posts';
 
 document.addEventListener('DOMContentLoaded', loadPosts);
 
-document.querySelector('.post-submit').addEventListener('click', submitPost)
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+document.querySelector('.card-body').addEventListener('click', enableEdit);
+document.querySelector('.card-body').addEventListener('click', cancelPost);
+document.getElementById('posts').addEventListener('click', deletePost);
+
 
 
 function loadPosts() {
@@ -16,26 +21,92 @@ function loadPosts() {
     .catch(err=>console.log(err))
 }
 
-function submitPost() {
+function submitPost(e) {
+   
     const title = document.getElementById('title').value;
     const body = document.getElementById('body').value;
+    const id = document.getElementById('id').value;
 
     const post = {
         title: title,
         body: body        
     }
+   
+        if(title === '' || body === '') {
+            ui.showAlert('All fields are required!', 'alert alert-danger')
+        } else {
+            if(id === '') {
+                fetch(httpAddress, {
+                    method: "POST",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(post)
+                })
+                .then(res=>res.json())
+                .then(()=>{
+                    ui.clearFields();
+                    ui.showAlert('Post added', 'alert alert-success');
+                    loadPosts()})
+                .catch(err=>console.log(err))
+            } else {
+                
+                fetch(`${httpAddress}/${id}`, {
+                    method: "PUT",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(post)
+                })
+                .then(res=>res.json())
+                .then(()=>{
+                    ui.clearFields();
+                    ui.showAlert('Post updated', 'alert alert-success');
+                    loadPosts()})
+                .catch(err=>console.log(err))
+              }     
+        }  
+    }
 
-    fetch(httpAddress, {
-        method: "POST",
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(post)
-    })
-    .then(res=>res.json())
-    .then(()=>{
-        ui.clearFields();
-        ui.showAlert('Post added', 'alert alert-success');
-        loadPosts()})
-    .catch(err=>console.log(err))
+function enableEdit(e) {
+    if(e.target.classList.contains('fa-edit')) {
+        const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+        const body = e.target.parentElement.previousElementSibling.textContent;
+        const id = e.target.parentElement.dataset.id;
+
+        const data = {
+            title,
+            body,
+            id
+        }
+
+        ui.editPost(data)
+    }
+}
+
+function cancelPost(e) {
+    if(e.target.classList.contains('post-cancel')) {
+        ui.changeState('add');
+    }
+}
+
+function deletePost(e) {
+   
+    if(e.target.classList.contains('fa-trash')) {
+        const id = e.target.parentElement.dataset.id;
+        e.target.parentElement.parentElement.parentElement.remove();
+
+        fetch(`${httpAddress}/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(res=>res.json())
+        .then(()=>{
+            ui.clearFields();
+            ui.showAlert('Post deleted', 'alert alert-danger');
+            loadPosts()})
+        .catch(err=>console.log(err))
+    }
 }
--- microposts/src/ui.js
+++ microposts/src/ui.js
@@ -31,6 +31,7 @@ class UI {
     }
 
     showAlert(message, className) {
+        if(!document.querySelector('.alert')) {
         const alert = document.createElement('div');
         alert.className = className;
         alert.textContent = message;
@@ -38,14 +39,47 @@ class UI {
         container.insertBefore(alert, this.post)
         setTimeout(()=> {
             document.querySelector('.alert').remove()
-        }, 1000)
+        }, 2000)
     }
-
-
+    }
 
     clearFields() {
         this.titleInput.value = '';
         this.bodyInput.value = '';
+    }
+
+    editPost(data) {
+        this.titleInput.value = data.title;
+        this.bodyInput.value = data.body;
+        this.id.value = data.id; 
+
+        this.changeState('edit');
+    }
+
+    changeState(type) {
+        if(type === 'edit') {
+          
+            this.postSubmit.classList = 'btn btn-warning btn-block post-submit';
+            this.postSubmit.value = 'Edit post';
+
+            
+                const button = document.createElement('button');
+                button.textContent = 'Cancel edit';
+                button.classList = 'btn btn-danger btn-block post-cancel';
+            
+           
+
+           const formEnd = document.querySelector('.form-end');
+           const cardForm = document.querySelector('.card-form');
+           cardForm.insertBefore(button, formEnd)
+           
+        } else {
+            this.postSubmit.classList = 'btn btn-success btn-block post-submit';
+            this.postSubmit.value = 'Post it';
+
+            if(document.querySelector('.post-cancel')) document.querySelector('.post-cancel').remove()
+            this.clearFields();
+        }
     }
 }
 
