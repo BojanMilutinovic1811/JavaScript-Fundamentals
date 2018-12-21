2f3dfbd5f0384a7b4483e5038f33ae74de7ba8b9
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-12-21 10:53:15 +0100

    exercise

---
+++ exercise/exercise.js
@@ -0,0 +1,27 @@
+// var a;
+// console.log(typeof a); undefined
+
+// var s = '1s'; 
+// s++;
+// console.log(s); NaN
+
+// console.log(!!"false"); true
+
+// console.log(!!undefined); false
+
+// console.log(typeof -Infinity);  number
+
+// console.log(10 % "0"); NaN
+
+// console.log(undefined == null); true
+
+// console.log(false === ""); false
+// console.log(false == ""); true
+
+// console.log(typeof "2E+2"); string 
+
+// a = 3e+3; a++;
+// console.log(a); 3001
+
+// var v = v || 10;
+// console.log(v); 10
